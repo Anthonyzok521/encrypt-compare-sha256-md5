501858166a6e0b34afa5eca614592c75872e9c0a
Anthonyzok521 <anthonyzok521@gmail.com>
2024-06-27 17:13:53 -0400

    feat: view engine ejs

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const ejs = require('ejs');
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 3000;
+
+app.set('views', './') // specify the views directory
+app.set('view engine', 'ejs') // register the template engine
+
+app.get('/', (req,res)=>{
+    res.render('index')
+})
 
 app.listen(PORT, ()=>{
     console.log(`Server on port ${PORT}`);
